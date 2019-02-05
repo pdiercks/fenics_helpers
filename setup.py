ae0c19cc65decb7fe07a02e14fad7425fe9cd80a
Christoph Pohl <christophpohl@gmx.com>
2019-02-05 10:58:23 +0100

    Add setup.py for installation with pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fenics_helpers",
+    version="0.1",
+    author="Christoph Pohl",
+    author_email="christoph.pohl@bam.de",
+    description="A tiny package to modify the node order of quadratic triangles and tets in a xdmf-hdf mesh to work with FEniCS/DOLFIN.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nutofem/fenics_helpers",
+    packages=["fenics_helpers"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
