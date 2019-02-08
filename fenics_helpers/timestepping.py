c57fe89e99d25a9d258eb9d8db489ff4626ac1b0
Thomas Titscher <thomas.titscher@gmail.com>
2019-02-08 08:42:11 +0100

    Refactor output in class Progress

--- fenics_helpers/timestepping.py
+++ fenics_helpers/timestepping.py
@@ -6,41 +6,38 @@ class Progress:
     def __init__(self, t_start, t_end, show_bar):
         self._show_bar = show_bar
 
-        self.TGREEN = "\033[32m"
-        self.TRED = "\033[31m"
-        self.TEND = "\033[m"
-
         if self._show_bar:
             from tqdm import tqdm
 
-            self._pbar = tqdm(
-                total=t_end - t_start, ascii=True, bar_format="{l_bar}{bar}{rate_fmt}"
-            )
+            fmt = "{l_bar}{bar}{rate_fmt}"
+            self._pbar = tqdm(total=t_end - t_start, ascii=True, bar_format=fmt)
+
+    def _green(self, msg):
+        info("\033[32m" + msg + "\033[m")
+
+    def _red(self, msg):
+        info("\033[31m" + msg + "\033[m")
+
+    def iteration_info(self, t, dt, iterations):
+        return "at t = {:8.5f} after {:2} iteration(s) with dt = {:8.5f}.".format(
+            t, iterations, dt
+        )
 
     def success(self, t, dt, iterations):
         if self._show_bar:
             self._pbar.update(dt)
             self._pbar.set_description("dt = {:8.5f}".format(dt))
         else:
-            info(
-                self.TGREEN
-                + "Convergence at t = {:8.5f} after {:2} iteration(s) with dt = {:8.5f}.".format(
-                    t, iterations, dt
-                )
-                + self.TEND
-            )
+            self._green("Convergence " + self.iteration_info(t, dt, iterations))
 
-    def error(self, t, dt):
+    def error(self, t, dt, iterations):
         if not self._show_bar:
-            info(
-                self.TRED
-                + "No convergence at t = {:8.5f} with dt = {:8.5f}.".format(t, dt)
-                + self.TEND
-            )
+            self._red("No Convergence " + self.iteration_info(t, dt, iterations))
 
     def __del__(self):
         if self._show_bar:
             self._pbar.close()
+
 
 class CheckPoints:
     def __init__(self, points, t_start, t_end, dt_min=1e-6):
@@ -64,10 +61,10 @@ class CheckPoints:
         return dt
 
     def reached(self, t):
-        if self.points.size !=0 and t == self.points[0]:
+        if self.points.size != 0 and t == self.points[0]:
             self.points = np.delete(self.points, 0)
 
-        
+
 class AdaptiveTimeStepping:
     def __init__(self, solve, post_process, u):
         assert isinstance(u, Function)
@@ -120,7 +117,7 @@ class AdaptiveTimeStepping:
                     if dt < self.dt_min:
                         return True
             else:
-                progress.error(t, dt)
+                progress.error(t, dt, num_iter)
 
                 self._u.assign(u_prev)
                 t -= dt
