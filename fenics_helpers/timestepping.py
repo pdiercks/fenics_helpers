Fix further floating point precision issues

fenics_helpers/timestepping.py
@@ -1,5 +1,6 @@
 import numpy as np
 from dolfin import Function, info
+import math
 
 
 def term_color(color_name):
@@ -8,8 +9,10 @@ def term_color(color_name):
     if color_name == "green":
         return "\033[32m"
 
+
 def colored(msg, color_name):
     return term_color(color_name) + msg + "\033[m"
+
 
 class ProgressInfo:
     def __init__(self, t_start, t_end):
@@ -28,6 +31,7 @@ class ProgressInfo:
         msg = "No convergence " + self.iteration_info(t, dt, iterations)
         info(colored(msg, "red"))
 
+
 class ProgressBar:
     def __init__(self, t_start, t_end):
         from tqdm import tqdm
@@ -45,6 +49,7 @@ class ProgressBar:
     def __del__(self):
         self._pbar.close()
 
+
 def get_progress(t_start, t_end, show_bar):
     return ProgressBar(t_start, t_end) if show_bar else ProgressInfo(t_start, t_end)
 
@@ -53,8 +58,6 @@ class CheckPoints:
     def __init__(self, points, t_start, t_end):
         self.points = np.sort(np.array(points))
 
-        if self.points.size == 0:
-            return
         if self.points.max() > t_end or self.points.min() < t_start:
             raise RuntimeError("Checkpoints outside of integration range.")
 
@@ -62,7 +65,9 @@ class CheckPoints:
         id_range = (self.points > t0) & (self.points < t1)
         points_within_dt = self.points[id_range]
         if points_within_dt.size != 0:
-            return points_within_dt[0]
+            for point_within_dt in points_within_dt:
+                if not math.isclose(point_within_dt, t0):
+                    return point_within_dt
 
     def timestep(self, t, dt):
         """
@@ -71,15 +76,16 @@ class CheckPoints:
         If there is such a point, the dt = t_check - t is returned.
         If nothing is found, the unmodified dt is returned.
         """
-        t_check = self._first_checkpoint_within(t, t+dt)
+        t_check = self._first_checkpoint_within(t, t + dt)
         if t_check:
             return t_check - t
         return dt
 
+
 class Adaptive:
     def __init__(self, solve, post_process, u):
         assert isinstance(u, Function)
-        self.dt_min = 1.e-6
+        self.dt_min = 1.0e-6
         self.dt_max = 0.1
         self.decrease_factor = 0.5
         self.increase_factor = 1.5
@@ -96,9 +102,11 @@ class Adaptive:
         t = t_start
         self._post_process(t)
 
-        progress = get_progress(t_start, t_end, show_bar) 
+        progress = get_progress(t_start, t_end, show_bar)
+
+        # Checkpoints are reached exactly. So we add t_end to the checkpoints.
+        checkpoints = np.append(np.array(checkpoints), t_end)
         checkpoints = CheckPoints(checkpoints, t_start, t_end)
-        
 
         dt0 = dt
         while t < t_end:
@@ -106,14 +114,14 @@ class Adaptive:
             # We keep track of two time steps. dt0 is the time step that
             # ignores the checkpoints. This is the one that is adapted upon
             # fast/no convergence. dt is smaller than dt0
-            assert(dt <= dt0)
+            assert dt <= dt0
             # and coveres checkpoints.
 
             t += dt
 
             num_iter, converged = self._solve(t)
-            assert(isinstance(converged, bool))
-            assert(type(num_iter) == int) # isinstance(False, int) is True...
+            assert isinstance(converged, bool)
+            assert type(num_iter) == int  # isinstance(False, int) is True...
 
             if converged:
                 progress.success(t, dt, num_iter)
@@ -123,24 +131,20 @@ class Adaptive:
                 # increase the time step for fast convergence
                 if dt == dt0 and num_iter < self.increase_num_iter and dt < self.dt_max:
                     dt0 *= self.increase_factor
-                    dt0 = min(dt, self.dt_max)
+                    dt0 = min(dt0, self.dt_max)
                     if not show_bar:
                         info("Increasing time step to dt = {}.".format(dt0))
-
-                # adjust dt to end at t_end
-                dt0 = min(dt0, t_end - t)
 
             else:
                 progress.error(t, dt, num_iter)
 
                 self._u.assign(u_prev)
                 t -= dt
-                dt *= self.decrease_factor
 
-                if dt == dt0: 
+                if dt == dt0:
                     dt0 *= self.decrease_factor
-                if not show_bar:
-                    info("Reduce time step to dt = {}.".format(dt0))
+                    if not show_bar:
+                        info("Reduce time step to dt = {}.".format(dt0))
                 if dt0 < self.dt_min:
                     info("Abort since dt({}) < dt_min({})".format(dt0, self.dt_min))
                     return False
@@ -153,15 +157,16 @@ class Equidistant:
         self._post_process = post_process
 
     def run(self, t_end, dt, t_start=0.0, checkpoints=[], show_bar=False):
-        progress = get_progress(t_start, t_end, show_bar) 
+        progress = get_progress(t_start, t_end, show_bar)
 
+        if checkpoints: # check range
+            CheckPoints(checkpoints, t_start, t_end)
         points_in_time = np.append(np.arange(t_start, t_end, dt), t_end)
-        checkpoints = CheckPoints(checkpoints, t_start, t_end)
-        points_in_time = np.append(points_in_time, checkpoints.points)
+        points_in_time = np.append(points_in_time, checkpoints)
 
-        for t in np.sort(points_in_time):
+        for t in np.sort(np.unique(points_in_time)):
             num_iter, converged = self._solve(t)
-            assert(isinstance(converged, bool))
+            assert isinstance(converged, bool)
 
             if converged:
                 progress.success(t, dt, num_iter)

tests/test_timestepping.py
@@ -9,41 +9,19 @@ import hypothesis.strategies as st
 from context import fenics_helpers
 
 
-class Solver:
-    def __init__(self):
-        self.t = 0
-        self.past_it = {}
-
-    def __call__(self, t):
-        dt = t - self.t
-        key = (t, dt)
-        value = self.past_it.get(key)
-        if value:
-            return 3, value
-
-        value = random.choice([True, True, False])
-        self.past_it[key] = value
-        if value:
-            self.t = t
-
-        return 3, value
-
-
 class TestEquidistant(unittest.TestCase):
     def setUp(self):
         solve = lambda t: (3, True)
         pp = lambda t: None
-        self.equidistant = fenics_helpers.timestepping.Equidistant(
-            solve, pp
-        )
+        self.equidistant = fenics_helpers.timestepping.Equidistant(solve, pp)
 
     def test_run(self):
         self.assertTrue(self.equidistant.run(5.0, 1.0))
-    
+
     def test_run_failed(self):
         self.equidistant._solve = lambda t: (3, False)
         self.assertFalse(self.equidistant.run(100.0, 1.0, show_bar=True))
-    
+
     def test_invalid_solve(self):
         self.equidistant._solve = lambda t: (3, "False")
         self.assertRaises(Exception, self.equidistant.run, 5.0, 1.0)
@@ -59,6 +37,9 @@ class TestEquidistant(unittest.TestCase):
 
         self.assertIn(0., visited_timesteps)
         self.assertIn(5., visited_timesteps)
+        
+        # check for duplicates
+        self.assertEqual(len(visited_timesteps), np.unique(visited_timesteps).size)
 
     @given(st.lists(st.floats(max_value=0.0, exclude_max=True), min_size=1))
     def test_too_low_checkpoints(self, checkpoints):
@@ -73,16 +54,14 @@ class TestEquidistant(unittest.TestCase):
 
 class TestAdaptive(unittest.TestCase):
     def setUp(self):
-        s = Solver()
         solve = lambda t: (3, random.choice([True, True, True, True, False]))
         pp = lambda t: None
         u = dolfin.Function(dolfin.FunctionSpace(dolfin.UnitIntervalMesh(10), "P", 1))
-        # self.adaptive = fenics_helpers.timestepping.Adaptive(solve, pp, u)
-        self.adaptive = fenics_helpers.timestepping.Adaptive(s, pp, u)
+        self.adaptive = fenics_helpers.timestepping.Adaptive(solve, pp, u)
 
     def test_run(self):
         self.assertTrue(self.adaptive.run(1.5, 1.0))
-    
+
     def test_invalid_solve_first_str(self):
         self.adaptive._solve = lambda t: ("3", True)
         self.assertRaises(Exception, self.adaptive.run, 1.5, 1.0)
@@ -98,19 +77,22 @@ class TestAdaptive(unittest.TestCase):
         self.adaptive._solve = lambda t: (3, "False")
         self.assertRaises(Exception, self.adaptive.run, 1.5, 1.0)
 
-
     @given(st.lists(st.floats(0.0, 1.5)))
     def test_checkpoints(self, checkpoints):
         visited_timesteps = []
         pp = lambda t: visited_timesteps.append(t)
         self.adaptive._post_process = pp
         self.assertTrue(self.adaptive.run(1.5, dt=1.0, checkpoints=checkpoints))
-        eps = np.finfo(float).eps # eps float
+
+        eps = np.finfo(float).eps  # eps float
         for checkpoint in checkpoints:
             self.assertTrue(np.isclose(visited_timesteps, checkpoint, atol=eps).any())
-       
+
         self.assertTrue(np.isclose(visited_timesteps, 0, atol=eps).any())
         self.assertTrue(np.isclose(visited_timesteps, 1.5, atol=eps).any())
+        
+        # check for duplicates
+        self.assertEqual(len(visited_timesteps), np.unique(visited_timesteps).size)
 
     @given(st.lists(st.floats(max_value=0.0, exclude_max=True), min_size=1))
     def test_too_low_checkpoints(self, checkpoints):