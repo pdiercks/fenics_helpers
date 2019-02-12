9dba0f7964654e857a0a1cd1c43448c49b02b899
Thomas Titscher <thomas.titscher@gmail.com>
2019-02-12 12:04:56 +0100

    Add adaptive plot and example

---
+++ fenics_helpers/plotting/adaptive_plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+class AdaptivePlot:
+    def __init__(self, fmt="-bx", ax=None):
+        plt.ion() # interactive on
+        if ax is None:
+            ax = plt.gca()
+
+        self._line, = ax.plot([0.0], [0.0], fmt)
+        self._xs = []
+        self._ys = []
+
+    def __call__(self, x, y):
+        """
+        Add the point (x,y) to the plot and rescales the axes.
+        """
+        self._xs.append(x)
+        self._ys.append(y)
+        self._line.set_data(self._xs, self._ys)
+        self.ax.relim()
+        self.ax.autoscale_view(True, True, True)
+        self.ax.figure.canvas.flush_events()
+
+    def keep(self):
+        """
+        Deactivates the interactive mode to show the plot.        
+        """
+        plt.ioff() # interactive off
+        plt.show()
+
+    @property
+    def ax(self):
+        return self._line.axes
+
---
+++ tests/example_adaptive_plot.py
@@ -0,0 +1,33 @@
+import time
+
+from context import fenics_helpers 
+from fenics_helpers.plotting.adaptive_plot import AdaptivePlot
+
+import math
+
+def single_plot():
+    p = AdaptivePlot("-gx")
+    p.ax.set_xlabel("iteration")
+    p.ax.set_ylabel("time [ms]")
+    dt = 0.
+    for i in range(1000):
+        t = time.time()
+        p(i, 1000 * dt)
+        dt = time.time() - t
+
+def combined_plot():
+    p1 = AdaptivePlot("-gx")
+    p2 = AdaptivePlot("-rx", ax=p1.ax.twinx())
+
+    for i in range(200):
+        t = i / 10.
+        p1(t, math.sin(t))
+        p2(t, math.cos(t))
+
+    p1.keep()
+
+
+if __name__ == "__main__":
+    # single_plot()
+    combined_plot()
+
